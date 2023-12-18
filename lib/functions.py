d9d8ebd49e92ca1b2a1c8e158107d99ba1d04ead
Devin Elliot Gomez <devinelliotgomez@gmail.com>
2023-12-18 09:49:52 -0600

    finished lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
+    print("Hello, programmer!")
     pass
 
 def greet(name):
+    print(f"Hello, {name}!")
     pass
 
 def greet_with_default(name="programmer"):
+    print(f"Hello, {name}!")
     pass
 
 def add(num1, num2):
-    pass
+    sum = num1 + num2
+    return sum
+
 
 def halve(number):
-    pass
+    halve = number / 2
+    return halve
